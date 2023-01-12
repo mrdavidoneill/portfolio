a7c81f4d8ace504d316d288b526dbe726d94c50d
David ONeill <mrdavidoneillll@gmail.com>
2023-01-12 21:51:21 +0100

    Add flowbite to content field and plugin Extend theme for site: - add playfair font - add animations -- blobs of circles -- fade in page -- flip page -- thumb and hand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,80 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+      },
+      backgroundImage: {
+        desk: "url('/images/desk.jpg')",
+      },
+      animation: {
+        blob: "blob 20s ease-in-out infinite ",
+        flip: "flip 0.3s ease-in-out ",
+        hand: "hand 0.3s ease-in-out ",
+        thumb: "thumb 0.3s ease-in-out ",
+        fadein: "fadein 0.5s ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0vh, 0vh) scale(2)",
+          },
+          "35%": {
+            transform: "translate(-15vh, -15vh) scale(1)",
+          },
+          "70%": {
+            transform: "translate(15vh, 15vh) scale(3)",
+          },
+          "100%": {
+            transform: "translate(0vh, 0vh) scale(2)",
+          },
+        },
+        fadein: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100%" },
+        },
+        flip: {
+          "0%": {
+            transform: "rotateX(180deg)",
+          },
+          "30%": {
+            transform: "rotateX(110deg)",
+          },
+          "100%": {
+            transform: "rotateX(0)",
+          },
+        },
+        thumb: {
+          "0%": {
+            transform: "rotate(0) translate(0vh, 0vh) scale(1)",
+          },
+          "30%": {
+            transform: "rotate(40deg) translate(40vh, -20vh) scale(1.5)",
+          },
+          "100%": {
+            transform: "rotate(0) translate(0vh, 0vh) scale(1)",
+          },
+        },
+        hand: {
+          "0%": {
+            transform: "rotate(0) translate(0vh, 0vh) scale(1)",
+          },
+          "50%": {
+            transform: "rotate(-20deg) translate(10vh, 10vh) scale(1.25)",
+          },
+          "100%": {
+            transform: "rotate(0) translate(0vh, 0vh) scale(1)",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
