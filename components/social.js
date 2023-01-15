189871ae3b8cd1ccec771ef255e20993ac8ebb14
David ONeill <mrdavidoneillll@gmail.com>
2023-01-15 14:08:40 +0100

    Change from Link to a tag - need new tabs when opening socials - no need for Link optimisations

--- components/social.js
+++ components/social.js
@@ -1,12 +1,11 @@
 import { Button } from "flowbite-react";
-import Link from "next/link";
 
 export default function Social({ href, title }) {
   return (
-    <Link className="flex justify-center p-1" href={href} target="_blank">
+    <a className="flex justify-center p-1" href={href} target="_blank">
       <Button outline={true} gradientDuoTone="purpleToBlue">
         {title}
       </Button>
-    </Link>
+    </a>
   );
 }
